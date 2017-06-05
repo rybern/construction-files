4bb1f590636224244c168ca6b7e9c8c43f6c90e0
Ryan Bernstein <ryanbernstein1@gmail.com>
2017-06-05 13:51:57 -0400

    Works, ready to upload for private use, not documented, naming needs work

---
+++ src/BuildConstructor.hs
@@ -0,0 +1,109 @@
+{-# LANGUAGE OverloadedStrings #-}
+module BuildConstructor
+  (
+    Constructor (..)
+  , EvalError (..)
+  , ParseError (..)
+  , buildValue
+  , buildValueFromFile
+  , Symbol
+  )
+where
+
+import Data.Text
+import Data.Text.IO as Text
+import qualified Data.Map as Map
+import ParseConstructor
+import Control.Monad
+
+-- everything in this project badly needs renaming
+
+data Constructor s = C0 s
+                   | SimpleString (Text -> Constructor s)
+                   | SimpleInt (Integer -> Constructor s)
+                   | SimpleFloat (Double -> Constructor s)
+                   | SimpleBool (Bool -> Constructor s)
+                   | Composer (s -> Constructor s)
+
+newtype EvalError = EvalError Text deriving Show
+
+buildValue :: [(Symbol, (Constructor s))]
+           -> Text
+           -> Either ParseError (Either EvalError s)
+buildValue constructors =
+  (buildValueFromParsed (Map.fromList constructors) <$>) . parseConstructors
+
+buildValueFromFile :: [(Symbol, (Constructor s))]
+                   -> FilePath
+                   -> IO (Either ParseError (Either EvalError s))
+buildValueFromFile constructors =
+  ((buildValueFromParsed (Map.fromList constructors) <$>) <$>) . parseConstructorFile
+
+buildValueFromParsed :: Map.Map Symbol (Constructor s)
+                     -> ( [(Symbol, (Symbol, [Expression]))]
+                        , (Symbol, [Expression]))
+                     -> Either EvalError s
+buildValueFromParsed constructors ([], final) = buildSingleValue constructors final
+buildValueFromParsed constructors ((var, constructor):restAssignments, final) = do
+  when (var `Map.member` constructors) . Left . EvalError $
+    "The variable name " `append` var `append` "is already in use"
+  value <- buildSingleValue constructors constructor
+  let constructors' = Map.insert var (C0 value) constructors
+  buildValueFromParsed constructors' (restAssignments, final)
+
+buildSingleValue :: Map.Map Symbol (Constructor s)
+                 -> (Symbol, [Expression])
+                 -> Either EvalError s
+buildSingleValue constructors (sym, exprs) = case sym `Map.lookup` constructors of
+                                               Just c -> consumeExpressions constructors c (sym, exprs)
+                                               Nothing -> Left . EvalError $
+                                                 "Unknown constructor or variable: " `append` sym
+
+consumeExpressions :: Map.Map Symbol (Constructor s)
+                   -> Constructor s
+                   -> (Symbol, [Expression])
+                   -> Either EvalError s
+consumeExpressions _ (C0 s) (_, []) = Right s
+consumeExpressions _ (C0 s) (sym, _) = Left . EvalError $
+  "Too many arguments to constructor " `append` sym
+
+consumeExpressions cs (SimpleString strFn) (sym, ((Str str):rest)) =
+  consumeExpressions cs (strFn str) (sym, rest)
+consumeExpressions _ (SimpleString _) (sym, (_:_)) = Left . EvalError $
+  "Constructor " `append` sym `append` " requires a string where another type was given"
+consumeExpressions _ (SimpleString _) (sym, []) = Left . EvalError $
+  "Constructor " `append` sym `append` " requires a string where nothing was given (too few arguments)"
+
+consumeExpressions cs (SimpleInt intFn) (sym, ((Itr i):rest)) =
+  consumeExpressions cs (intFn i) (sym, rest)
+consumeExpressions _ (SimpleInt _) (sym, (_:_)) = Left . EvalError $
+  "Constructor " `append` sym `append` " requires an integer where another type was given"
+consumeExpressions _ (SimpleInt _) (sym, []) = Left . EvalError $
+  "Constructor " `append` sym `append` " requires an integer where nothing was given (too few arguments)"
+
+consumeExpressions cs (SimpleFloat floatFn) (sym, ((Flt x):rest)) =
+  consumeExpressions cs (floatFn x) (sym, rest)
+consumeExpressions cs (SimpleFloat floatFn) (sym, ((Itr i):rest)) =
+  consumeExpressions cs (floatFn (fromIntegral i)) (sym, rest)
+consumeExpressions _ (SimpleFloat _) (sym, (_:_)) = Left . EvalError $
+  "Constructor " `append` sym `append` " requires a float where another type was given"
+consumeExpressions _ (SimpleFloat _) (sym, []) = Left . EvalError $
+  "Constructor " `append` sym `append` " requires a float where nothing was given (too few arguments)"
+
+consumeExpressions cs (SimpleBool boolFn) (sym, ((Bol b):rest)) =
+  consumeExpressions cs (boolFn b) (sym, rest)
+consumeExpressions _ (SimpleBool _) (sym, (_:_)) = Left . EvalError $
+  "Constructor " `append` sym `append` " requires a boolean (True or False) where another type was given"
+consumeExpressions _ (SimpleBool _) (sym, []) = Left . EvalError $
+  "Constructor " `append` sym `append` " requires a boolean (True or False) where nothing was given (too few arguments)"
+
+consumeExpressions constructors (Composer sFn) (sym, ((Sym b):rest)) = do
+  argS <- buildSingleValue constructors (b, [])
+  consumeExpressions constructors (sFn argS) (sym, rest)
+consumeExpressions constructors (Composer sFn) (sym, ((Paren exprs):rest)) = do
+  argS <- buildSingleValue constructors (sym, exprs)
+  consumeExpressions constructors (sFn argS) (sym, rest)
+consumeExpressions _ (Composer _) (sym, (_:_)) = Left . EvalError $
+  "Constructor " `append` sym `append` " requires a Value where another type was given"
+consumeExpressions _ (Composer _) (sym, []) = Left . EvalError $
+  "Constructor " `append` sym `append` " requires a Value where nothing was given (too few arguments)"
---
+++ src/ParseConstructor.hs
@@ -0,0 +1,138 @@
+{-# LANGUAGE OverloadedStrings #-}
+module ParseConstructor where
+
+import Data.Attoparsec.Text
+import Data.Char
+import Data.Text hiding (map, any)
+import Data.Text as Text (unlines)
+import Data.Text.IO as Text (readFile)
+import Data.Scientific hiding (scientific)
+import qualified Data.Set as Set
+
+-- call it ConstructionParse or ConstructionLang or ConstructionFile
+-- need additional cases for IO wrapped, lists of various types
+-- everything can be done in this library, to the point where you can have a defaultMain function that takes the list of constructors and an action to do with the final value
+
+type Symbol = Text
+
+data Expression = Sym Symbol
+                | Paren [Expression]
+                | Str Text
+                | Flt Double
+                | Itr Integer
+                | Bol Bool
+                deriving (Show)
+
+newtype ParseError = ParseError Text deriving Show
+
+parseConstructorFile :: FilePath -> IO (Either ParseError ( [(Symbol, (Symbol, [Expression]))]
+                                                          , (Symbol, [Expression])))
+parseConstructorFile = (parseConstructors <$>) . Text.readFile
+
+parseConstructors :: Text -> Either ParseError ( [(Symbol, (Symbol, [Expression]))]
+                                               , (Symbol, [Expression]))
+parseConstructors = packErrors . eitherResult . finish . parse parseFile
+
+parseFile :: Parser ( [(Symbol, (Symbol, [Expression]))]
+                    , (Symbol, [Expression]) )
+parseFile = do
+  skipLines
+  assignments <- many' (withLines1 parseAssignment)
+  finalConstructor <- parseConstructor
+  skipLines
+  endOfInput
+  return (assignments, finalConstructor)
+
+finish :: IResult Text r -> IResult Text r
+finish = flip feed (pack "")
+
+parseAssignment :: Parser (Symbol, (Symbol, [Expression]))
+parseAssignment = do
+  var <- withSpaces parseSymbol
+  _ <- char '='
+  skipSpace
+  constructor <- parseConstructor
+  return (var, constructor)
+
+parseConstructor :: Parser (Symbol, [Expression])
+parseConstructor = do
+  identifier <- withSpaces parseSymbol
+  rest <- parseExpressions
+  return (identifier, rest)
+
+parseSymbol :: Parser Symbol
+parseSymbol = takeWhile1 isAlpha
+
+parseExpressions :: Parser [Expression]
+parseExpressions = many' (withSpaces parseExpression)
+
+parseExpressions1 :: Parser [Expression]
+parseExpressions1 = many1 (withSpaces parseExpression)
+
+parseExpression :: Parser Expression
+parseExpression = choice [
+    (Paren <$> parseParenthetical)
+  , parseExprNumber
+  , (Bol <$> parseBool)
+  , (Str <$> parseQuoted)
+  , (Sym <$> parseSymbol)
+  ]
+
+parseParenthetical :: Parser [Expression]
+parseParenthetical = do
+  _ <- char '('
+  skipSpace
+  exprs <- parseExpressions1
+  _ <- char ')'
+  return exprs
+
+parseExprNumber :: Parser Expression
+parseExprNumber = do
+  num <- scientific
+  return $ case floatingOrInteger num of
+    (Left d) -> Flt d
+    (Right i) -> Itr i
+
+parseBool :: Parser Bool
+parseBool = choice [
+    string "True" >> return True
+  , string "False" >> return False
+  ]
+
+parseQuoted :: Parser Text
+parseQuoted = do
+  _ <- char '"'
+  str <- takeWhile1 (/= '"')
+  _ <- char '"'
+  return str
+
+-- like isSpace but doesn't match newlines
+isSpace' :: Char -> Bool
+isSpace' c = any (c ==) [' ', '\t']
+
+skipSpace' :: Parser ()
+skipSpace' = skipWhile isSpace'
+
+withSpaces :: Parser a -> Parser a
+withSpaces = (<* skipSpace')
+
+skipLines :: Parser ()
+skipLines = skipWhile isEndOfLine
+
+withLines :: Parser a -> Parser a
+withLines = (<* skipLines)
+
+withLines1 :: Parser a -> Parser a
+withLines1 = withLines . (<* endOfLine)
+
+packErrors :: Either String a -> Either ParseError a
+packErrors (Right r) = Right r
+packErrors (Left s) = Left . ParseError $ pack s
+
+
+testFile = Text.unlines [ "a = b"
+                        , "b = c 1"
+                        , "b"
+                        ]
+
+p = finish $ parse parseFile testFile
