088d9243caca9a6af1730bfe0d9f2a6cc6fa82cd
Ryan Bernstein <ryanbernstein1@gmail.com>
2017-06-05 20:59:18 -0400

    added lists of ints, floats, and values. Also added monad handling, so any monadic action can be performed during evaluation.

--- src/BuildConstructor.hs
+++ src/BuildConstructor.hs
@@ -6,7 +6,10 @@ module BuildConstructor
   , ParseError (..)
   , buildValue
   , buildValueFromFile
+  , buildValueM
+  , buildValueFromFileM
   , Symbol
+  , eitherT
   )
 where
 
@@ -15,7 +18,10 @@ import Data.Text as Text (unlines)
 import Data.Text.IO as Text
 import qualified Data.Map as Map
 import ParseConstructor
+import Data.Functor.Identity
 import Control.Monad
+import Control.Monad.Trans.Class (lift)
+import Control.Monad.Trans.Either
 
 -- everything in this project badly needs renaming
                 -- | ListOfItr [Integer]
@@ -24,116 +30,132 @@ import Control.Monad
 
 -- should switch to making sure that each expression starts with a symbol in the evaluation stage instead of the parsing stage. this would be a useful function when evaluating nested expressions.
 
-data Constructor s = C0 s
-                   | SimpleString (Text -> Constructor s)
-                   | SimpleInt (Integer -> Constructor s)
-                   | SimpleFloat (Double -> Constructor s)
-                   | SimpleBool (Bool -> Constructor s)
-                   | SimpleListOfInt ([Integer] -> Constructor s)
-                   | SimpleListOfFloat ([Double] -> Constructor s)
-                   | SimpleListOfValue ([s] -> Constructor s)
-                   | Composer (s -> Constructor s)
+data Constructor m s = C0 s
+                     | SimpleString (Text -> Constructor m s)
+                     | SimpleInt (Integer -> Constructor m s)
+                     | SimpleFloat (Double -> Constructor m s)
+                     | SimpleBool (Bool -> Constructor m s)
+                     | SimpleListOfInt ([Integer] -> Constructor m s)
+                     | SimpleListOfFloat ([Double] -> Constructor m s)
+                     | SimpleListOfValue ([s] -> Constructor m s)
+                     | Composer (s -> Constructor m s)
+                     | Monadic (m (Constructor m s))
 
 newtype EvalError = EvalError Text deriving Show
 
-buildValue :: [(Symbol, (Constructor s))]
+buildValue :: [(Symbol, (Constructor Identity s))]
            -> Text
-           -> Either ParseError (Either EvalError s)
-buildValue constructors =
+           -> Either ParseError (EitherT EvalError Identity s)
+buildValue = buildValueM
+
+buildValueFromFile :: [(Symbol, (Constructor Identity s))]
+                    -> FilePath
+                    -> IO (Either ParseError (EitherT EvalError Identity s))
+buildValueFromFile = buildValueFromFileM
+
+buildValueM :: (Monad m)
+            => [(Symbol, (Constructor m s))]
+            -> Text
+            -> Either ParseError (EitherT EvalError m s)
+buildValueM constructors =
   (buildValueFromParsed (Map.fromList constructors) <$>) . parseConstructors
 
-buildValueFromFile :: [(Symbol, (Constructor s))]
+buildValueFromFileM :: (Monad m)
+                   => [(Symbol, (Constructor m s))]
                    -> FilePath
-                   -> IO (Either ParseError (Either EvalError s))
-buildValueFromFile constructors =
+                   -> IO (Either ParseError (EitherT EvalError m s))
+buildValueFromFileM constructors =
   ((buildValueFromParsed (Map.fromList constructors) <$>) <$>) . parseConstructorFile
 
-buildValueFromParsed :: Map.Map Symbol (Constructor s)
+buildValueFromParsed :: (Monad m)
+                     => Map.Map Symbol (Constructor m s)
                      -> ( [(Symbol, (Symbol, [Expression]))]
                         , (Symbol, [Expression]))
-                     -> Either EvalError s
+                     -> EitherT EvalError m s
 buildValueFromParsed constructors ([], final) = buildSingleValue constructors final
 buildValueFromParsed constructors ((var, constructor):restAssignments, final) = do
-  when (var `Map.member` constructors) . Left . EvalError $
+  when (var `Map.member` constructors) . left . EvalError $
     "The variable name " `append` var `append` "is already in use"
   value <- buildSingleValue constructors constructor
   let constructors' = Map.insert var (C0 value) constructors
   buildValueFromParsed constructors' (restAssignments, final)
 
-buildSingleValue :: Map.Map Symbol (Constructor s)
+buildSingleValue :: (Monad m)
+                 => Map.Map Symbol (Constructor m s)
                  -> (Symbol, [Expression])
-                 -> Either EvalError s
+                 -> EitherT EvalError m s
 buildSingleValue constructors (sym, exprs) = case sym `Map.lookup` constructors of
                                                Just c -> consumeExpressions constructors c (sym, exprs)
-                                               Nothing -> Left . EvalError $
+                                               Nothing -> left . EvalError $
                                                  "Unknown constructor or variable: " `append` sym
 
-consumeExpressions :: Map.Map Symbol (Constructor s)
-                   -> Constructor s
+consumeExpressions :: (Monad m)
+                   => Map.Map Symbol (Constructor m s)
+                   -> Constructor m s
                    -> (Symbol, [Expression])
-                   -> Either EvalError s
-consumeExpressions _ (C0 s) (_, []) = Right s
-consumeExpressions _ (C0 s) (sym, _) = Left . EvalError $
+                   -> EitherT EvalError m s
+consumeExpressions _ (C0 s) (_, []) = right s
+consumeExpressions _ (C0 s) (sym, _) = left . EvalError $
   "Too many arguments to constructor " `append` sym
 
 consumeExpressions cs (SimpleString strFn) (sym, ((Str str):rest)) =
   consumeExpressions cs (strFn str) (sym, rest)
-consumeExpressions _ (SimpleString _) (sym, (_:_)) = Left . EvalError $
+consumeExpressions _ (SimpleString _) (sym, (_:_)) = left . EvalError $
   "Constructor " `append` sym `append` " requires a string where another type was given"
-consumeExpressions _ (SimpleString _) (sym, []) = Left . EvalError $
+consumeExpressions _ (SimpleString _) (sym, []) = left . EvalError $
   "Constructor " `append` sym `append` " requires a string where nothing was given (too few arguments)"
 
 consumeExpressions cs (SimpleInt intFn) (sym, ((Itr i):rest)) =
   consumeExpressions cs (intFn i) (sym, rest)
-consumeExpressions _ (SimpleInt _) (sym, (_:_)) = Left . EvalError $
+consumeExpressions _ (SimpleInt _) (sym, (_:_)) = left . EvalError $
   "Constructor " `append` sym `append` " requires an integer where another type was given"
-consumeExpressions _ (SimpleInt _) (sym, []) = Left . EvalError $
+consumeExpressions _ (SimpleInt _) (sym, []) = left . EvalError $
   "Constructor " `append` sym `append` " requires an integer where nothing was given (too few arguments)"
 
 consumeExpressions cs (SimpleFloat floatFn) (sym, ((Flt x):rest)) =
   consumeExpressions cs (floatFn x) (sym, rest)
 consumeExpressions cs (SimpleFloat floatFn) (sym, ((Itr i):rest)) =
   consumeExpressions cs (floatFn (fromIntegral i)) (sym, rest)
-consumeExpressions _ (SimpleFloat _) (sym, (_:_)) = Left . EvalError $
+consumeExpressions _ (SimpleFloat _) (sym, (_:_)) = left . EvalError $
   "Constructor " `append` sym `append` " requires a float where another type was given"
-consumeExpressions _ (SimpleFloat _) (sym, []) = Left . EvalError $
+consumeExpressions _ (SimpleFloat _) (sym, []) = left . EvalError $
   "Constructor " `append` sym `append` " requires a float where nothing was given (too few arguments)"
 
 consumeExpressions cs (SimpleBool boolFn) (sym, ((Bol b):rest)) =
   consumeExpressions cs (boolFn b) (sym, rest)
-consumeExpressions _ (SimpleBool _) (sym, (_:_)) = Left . EvalError $
+consumeExpressions _ (SimpleBool _) (sym, (_:_)) = left . EvalError $
   "Constructor " `append` sym `append` " requires a boolean (True or False) where another type was given"
-consumeExpressions _ (SimpleBool _) (sym, []) = Left . EvalError $
+consumeExpressions _ (SimpleBool _) (sym, []) = left . EvalError $
   "Constructor " `append` sym `append` " requires a boolean (True or False) where nothing was given (too few arguments)"
 
 consumeExpressions cs (SimpleListOfInt liFn) (sym, ((ListOfItr is):rest)) =
   consumeExpressions cs (liFn is) (sym, rest)
-consumeExpressions _ (SimpleListOfInt _) (sym, (_:_)) = Left . EvalError $
+consumeExpressions _ (SimpleListOfInt _) (sym, (_:_)) = left . EvalError $
   "Constructor " `append` sym `append` " requires a list of integers where another type was given"
-consumeExpressions _ (SimpleListOfInt _) (sym, []) = Left . EvalError $
+consumeExpressions _ (SimpleListOfInt _) (sym, []) = left . EvalError $
   "Constructor " `append` sym `append` " requires a list of integers where nothing was given (too few arguments)"
 
 consumeExpressions cs (SimpleListOfFloat lxFn) (sym, ((ListOfFlt xs):rest)) =
   consumeExpressions cs (lxFn xs) (sym, rest)
 consumeExpressions cs (SimpleListOfFloat lxFn) (sym, ((ListOfItr is):rest)) =
   consumeExpressions cs (lxFn (Prelude.map fromIntegral is)) (sym, rest)
-consumeExpressions _ (SimpleListOfFloat _) (sym, (_:_)) = Left . EvalError $
+consumeExpressions _ (SimpleListOfFloat _) (sym, (_:_)) = left . EvalError $
   "Constructor " `append` sym `append` " requires a list of floats where another type was given"
-consumeExpressions _ (SimpleListOfFloat _) (sym, []) = Left . EvalError $
+consumeExpressions _ (SimpleListOfFloat _) (sym, []) = left . EvalError $
   "Constructor " `append` sym `append` " requires a list of floats where nothing was given (too few arguments)"
 
 consumeExpressions constructors (SimpleListOfValue lexprFn) (sym, ((ListOfExpr exprs):rest)) = do
   values <- forM exprs $ \expr -> case expr of
     Sym b -> buildSingleValue constructors (b, [])
     Paren (Sym b:args) -> buildSingleValue constructors (b, args)
-    otherwise -> Left . EvalError $
+    otherwise -> left . EvalError $
       "Constructor "
       `append` sym `append`
       " requires a list of Values (variables or constructors), but another type was given in the list"
   consumeExpressions constructors (lexprFn values) (sym, rest)
-consumeExpressions _ (SimpleListOfValue _) (sym, (_:_)) = Left . EvalError $
+consumeExpressions _ (SimpleListOfValue _) (sym, (_:_)) = left . EvalError $
   "Constructor " `append` sym `append` " requires a list of Values where another type was given"
-consumeExpressions _ (SimpleListOfValue _) (sym, []) = Left . EvalError $
+consumeExpressions _ (SimpleListOfValue _) (sym, []) = left . EvalError $
   "Constructor " `append` sym `append` " requires a list of Values where nothing was given (too few arguments)"
 
 consumeExpressions constructors (Composer sFn) (sym, (Sym b):rest) = do
@@ -142,22 +164,30 @@ consumeExpressions constructors (Composer sFn) (sym, (Sym b):rest) = do
 consumeExpressions constructors (Composer sFn) (sym, (Paren ((Sym b):exprs)):rest) = do
   argS <- buildSingleValue constructors (b, exprs)
   consumeExpressions constructors (sFn argS) (sym, rest)
-consumeExpressions _ (Composer _) (sym, (_:_)) = Left . EvalError $
+consumeExpressions _ (Composer _) (sym, (_:_)) = left . EvalError $
   "Constructor " `append` sym `append` " requires a Value where another type was given"
-consumeExpressions _ (Composer _) (sym, []) = Left . EvalError $
+consumeExpressions _ (Composer _) (sym, []) = left . EvalError $
   "Constructor " `append` sym `append` " requires a Value where nothing was given (too few arguments)"
+
+consumeExpressions constructors (Monadic op) exprs =
+  lift op >>= \res -> consumeExpressions constructors res exprs
 
 {-
 some emergency test code
 
+ - }
+
 cs = [ ("id" :: Symbol, SimpleListOfFloat C0)
      , ("join", SimpleListOfValue (C0 . Prelude.concat))
-     , ("singleton", SimpleFloat (\x -> C0 [x]))]
+     , ("singleton", SimpleFloat (\x -> C0 [x]))
+     , ("readin", Monadic (readLn >>= \x -> return $ C0 [x]))]
 
-test = Text.unlines
+testLines = Text.unlines
   [
     "a = id [1, 2, 3, 54, 5, 6.1234, 6]"
   , "b = id [123, 3, 54, 5, 6, 3, 4, 2]"
-  , "join [(singleton 45.56), (join [a, b, (id [123, 4,2134, 5])])]"
+  , "join [(singleton 45.56), (join [a, b, (id [123, 4,2134, 5]), (readin)])]"
   ]
+
+test = buildValueM cs testLines
 -}
